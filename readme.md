e0c692800f3ec5128e4251da134f5d450fcda9e0
ViktoriiaRepo <samarityankav@gmail.com>
2024-01-30 09:55:27 +0200

    Add flexbox

---
+++ readme.md
@@ -0,0 +1,16 @@
+<h1 align='center'>Flexbox</h1>
+
+##Description
+
+This repository contains a simple website designed for practicing and mastering the fundamentals of Flexbox layout. Flexbox, or the Flexible Box Model, is a powerful CSS layout mechanism that provides an efficient way to design responsive and dynamic user interfaces.
+
+<img src='./img/align-items.png> width="100%"'>
+<img src='./img/align-items.png width="100%"'>
+<img src='./img/align-items.png width="100%"'>
+<img src='./img/align-items.png width="100%"'>
+
+##Tools
+
+- HTML5,
+- CSS,
+- JS
--- script.js
+++ script.js
@@ -1,9 +1,37 @@
 "use strict";
-
-function changeFlexDirection(value) {
+function changeJustifyContent(value) {
   const container = document.querySelector(".container");
+  container.style.justifyContent = value;
+  const buttons = document.querySelectorAll(".nav .flex");
+
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  event.target.classList.add("active");
+}
+
+function changeAlignItemsContent(value) {
+  const container = document.querySelector(".row-wrapper");
+  container.style.alignItems = value;
+  const buttons = document.querySelectorAll(".nav .flex");
+
+  buttons.forEach((button) => {
+    button.classList.remove("active");
+  });
+
+  event.target.classList.add("active");
+}
+
+function changeOrder(value) {
+  const container = document.querySelector("#flex-direction");
   container.style.flexDirection = value;
-  const buttons = document.querySelectorAll(".flex-direction");
+  if (value === "column" || value === "column-reverse") {
+    container.classList.add("column");
+  } else {
+    container.classList.remove("column");
+  }
+  const buttons = document.querySelectorAll(".nav .flex");
 
   buttons.forEach((button) => {
     button.classList.remove("active");
